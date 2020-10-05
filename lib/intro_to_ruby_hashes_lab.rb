17678df0be85f3a9ca71316ba209e5a135ad51f7
Amelia Elton <ameliareneeelton@gmail.com>
2020-10-05 18:10:05 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -3,7 +3,10 @@ def new_hash
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  my_hash = {
+    "Bananas" => 6
+    "Strawberries" => 1 
+  }
 end
 
 def pioneer
